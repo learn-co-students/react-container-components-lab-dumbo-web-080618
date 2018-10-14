593b1bdb0c055b9ae6ebd7553915ac9c5c3a3acd
github username <github email address>
2018-10-14 11:35:05 -0400

    working on cofig

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const MovieReviews = ({ text }) => {
-  
+
   return( <div className="review-list">
-    <h2>{text.display_title}</h2>
+    <h2>{text.display_title}</h2> 
     <h2>{text.headline}</h2>
     <h4>{text.byline}</h4>
     <p>{text.date_updated}</p>
