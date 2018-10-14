3af349bcdce3d3867e2a4a94e9c266ab4798ed04
github username <github email address>
2018-10-14 18:00:36 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-// const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const NYT_API_KEY = 'a40bd4c26c81442097ee42d440008f5c';
+
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const MovieReviews = ({ text }) => {
-  // debugger
   return( <div className="review-list">
     <h2>{text.display_title}</h2>
     <h2>{text.headline}</h2>
@@ -16,8 +15,3 @@ const MovieReviews = ({ text }) => {
 }
 
 export default MovieReviews;
-
-
-// Your MovieReviews component should be stateless and functional.
-//
-// You are free to lay out each review as you like using the data that the API returns, but make sure that the component outputs a top-level element with the class review-list and that each review is wrapped by an element with class review.
