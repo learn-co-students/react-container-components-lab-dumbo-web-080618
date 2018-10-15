974d5d6c666041ba75c116d5b5048ba1f86e54ec
github username <github email address>
2018-10-15 09:37:07 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,25 @@
 // Code MovieReviews Here
+import React, { Component } from 'react';
+
+const MovieReviews = props => {
+  return (
+  <div className="review-list">
+    {props.reviews.map((news, id) => <Review key={id} news={news}/>)}
+  </div>
+)}
+
+const Review = props =>{
+  return (
+    <div className="review">
+      <h1>{props.news.display_title}</h1>
+      <h2>{props.news.headline}</h2>
+    </div>
+  )
+}
+
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
+
+export default MovieReviews
