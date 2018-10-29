788847984963a7d65e978356c29f479e4ea98259
mikepcarlos <michaelpcarlos@gmail.com>
2018-10-29 15:03:27 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,21 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = (props) =>{
+
+
+const movieCard = props.reviews.map(review => {
+  return(
+  <ul className="review">
+    <h1> HEADLINE -- {review.headline}</h1>
+  </ul>
+)
+})
+
+return(
+<div className="review-list">
+  {movieCard}
+</div>
+)}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,68 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+class SearchableMovieReviewsContainer extends Component {
+
+  constructor(){
+    super()
+
+    this.state = {
+      reviews: [],
+      searchTerm: ""
+    }
+  }
+
+  fetchReviews = () => {
+    fetch(URL)
+      .then(res => res.json())
+      .then(reviews => this.filterReviews(reviews.results))
+  }
+
+
+  handleChange = (event) => {
+    this.setState({
+      searchTerm: event.target.value
+    })
+  }
+
+  // handleSubmit = (event) => {
+  //   event.preventDefault()
+  //   fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${this.state.searchTerm}`)
+  //     .then(res => res.json())
+  //     .then(reviews => {
+  //       let newReviews = reviews.results.filter(review => review.headline.includes(this.state.searchTerm))
+  //
+  //       this.setState({
+  //         reviews: newReviews
+  //       })
+  //     })
+  // }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${this.state.searchTerm}`)
+      .then(res => res.json())
+      .then(reviews => this.setState({
+        reviews: reviews.results
+      }))
+  }
+
+  render(){
+    return(
+      <div className="searchable-movie-reviews">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            id="search-input"
+            type="text"
+            style={{ width: 300 }}
+            onChange={this.handleChange}/>
+          <button type="submit">Submit</button>
+        </form>
+        <MovieReviews reviews={this.state.reviews}/>
+      </div>
+    )
+  }
+}
+
+export default SearchableMovieReviewsContainer
